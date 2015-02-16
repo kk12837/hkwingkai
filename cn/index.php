f02b2eb0c11830ba3fb1de64347ecd82a328e29d
keithbmcd <keith@bmcd.co>
2015-02-16 22:18:47 +0800

    update

---
+++ cn/index.php
@@ -0,0 +1,386 @@
+<?php date_default_timezone_set('America/Los_Angeles'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="1988年在香港成立，同年在深圳設廠生產PVC 膠粒，2006年在廣東惠州市鎮隆鎮自置廠房以一流的配方設計，優良的生產設備，優秀的一線員工，專業生產環保型pvc玩具膠粒. 產品專供港資廠生產玩具出口歐美及世界各地.">
+    <meta name="keywords" content="香港永佳實業有限公司, pvc 膠粒, 玩具膠粒, pvc">
+    <meta name="author" content="">
+    <!--google tracking -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-32317730-4', 'hongkongwingkai.com');
+        ga('send', 'pageview');
+
+    </script>
+    <title>香港永佳實業有限公司</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/stylish-portfolio.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
+
+    <!-- Add jQuery library -->
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+    <!-- Add mousewheel plugin (this is optional) -->
+    <script type="text/javascript" src="fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+
+    <!-- Add fancyBox -->
+    <link rel="stylesheet" href="fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+    <script type="text/javascript" src="fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
+
+    <!-- Optionally add helpers - button, thumbnail and/or media -->
+    <link rel="stylesheet" href="fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
+    <script type="text/javascript" src="fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+    <script type="text/javascript" src="fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.6"></script>
+
+    <link rel="stylesheet" href="fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+    <script type="text/javascript" src="fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle"><i class="fa fa-bars"></i></a>
+    <nav id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle"><i class="fa fa-times"></i></a>
+            <li class="sidebar-brand">
+                <a href="#top">香港永佳實業有限公司</a>
+            </li>
+            <li>
+                <a href="#top">主頁</a>
+            </li>
+            <li>
+                <a href="../index.php">English</a>
+            </li>
+            <li>
+                <a href="#about">關於我們</a>
+            </li>
+            <li>
+                <a href="#partners">合作伙伴</a>
+            </li>
+            <li>
+                <a href="#portfolio">主要產品</a>
+            </li>
+            <li>
+                <a href="#contact">聯絡我們</a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- Header -->
+    <header id="top" class="header">
+        <div class="text-vertical-center">
+            <h1>香港永佳實業有限公司</h1>
+<!--            <h3>Free Bootstrap Themes &amp; Templates</h3>-->
+            <br>
+            <a href="#about" class="btn btn-dark btn-lg">關於我們</a>
+        </div>
+    </header>
+
+    <!-- About -->
+    <section id="about" class="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>公司簡介</h2>
+                    <p>香港永佳實業有限公司， 1988年在香港成立，同年在深圳設廠生產PVC 膠粒，2006年在廣東惠州市鎮隆鎮自置廠房以一流的配方設計，優良的生產設備，優秀的一線員工，專業生產環保型pvc玩具膠粒. 產品專供港資廠生產玩具出口歐美及世界各地. 公司的誠信，企業的實力獲得了廣大老客戶的認可，熱誠歡迎業界的新老客戶到本廠洽談，參觀考察並指導！</p>
+
+                    <p>廣東惠州市.鎮隆鎮. 黃洞工業區廠: 興永佳塑膠製品(惠州)有限公司</p>
+                    <a class="fancybox" rel="group" href="img/company/003_small.jpg">
+                        <img class="about_img" src="img/company/003_small.jpg">
+                    </a>
+                    <a class="fancybox" rel="group" href="img/company/007_small.jpg">
+                        <img class="about_img" src="img/company/007_small.jpg">
+                    </a>
+                    <a class="fancybox" rel="group" href="img/company/008_small.jpg">
+                        <img class="about_img" src="img/company/008_small.jpg">
+                    </a>
+                    <a class="fancybox" rel="group" href="img/company/015_small.jpg">
+                        <img class="about_img" src="img/company/015_small.jpg">
+                    </a>
+
+                    <br>
+
+                    <h3>廠內環境和設施</h3>
+                    <img class="about_img" src="img/company/inside/1.PNG">
+                    <img class="about_img" src="img/company/inside/2.PNG">
+                    <img class="about_img" src="img/company/inside/3.PNG">
+                    <img class="about_img" src="img/company/inside/4.PNG">
+                    <img class="about_img" src="img/company/inside/5.PNG">
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+
+    <!-- Services -->
+    <!-- The circle icons use Font Awesome's stacked icon classes. For more information, visit http://fontawesome.io/examples/ -->
+    <section id="partners" class="services bg-primary">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-lg-10 col-lg-offset-1">
+                    <h2>合作伙伴</h2>
+                    <hr class="small">
+                    <!--<div class="row">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="service-item">
+                                <span class="fa-stack fa-4x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-cloud fa-stack-1x text-primary"></i>
+                            </span>
+                                <h4>
+                                    <strong>Service Name</strong>
+                                </h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                <a href="#" class="btn btn-light">Learn More</a>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="service-item">
+                                <span class="fa-stack fa-4x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-compass fa-stack-1x text-primary"></i>
+                            </span>
+                                <h4>
+                                    <strong>Service Name</strong>
+                                </h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                <a href="#" class="btn btn-light">Learn More</a>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="service-item">
+                                <span class="fa-stack fa-4x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-flask fa-stack-1x text-primary"></i>
+                            </span>
+                                <h4>
+                                    <strong>Service Name</strong>
+                                </h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                <a href="#" class="btn btn-light">Learn More</a>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="service-item">
+                                <span class="fa-stack fa-4x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-shield fa-stack-1x text-primary"></i>
+                            </span>
+                                <h4>
+                                    <strong>Service Name</strong>
+                                </h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                <a href="#" class="btn btn-light">Learn More</a>
+                            </div>
+                        </div>
+                    </div>-->
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.col-lg-10 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+
+    <!-- Callout -->
+<!--    <aside class="callout">-->
+<!--        <div class="text-vertical-center">-->
+<!--            <h1>Vertically Centered Text</h1>-->
+<!--        </div>-->
+<!--    </aside>-->
+
+    <!-- Portfolio -->
+    <section id="portfolio" class="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                    <h2>主要產品</h2>
+                    <hr class="small">
+                    <div class="row">
+                        <h3>PVC 膠粒</h3>
+                        <a class="fancybox" rel="group" href="img/products/product1.PNG">
+                            <img class="product_img img-portfolio img-responsive" src="img/products/product1.PNG">
+                        </a>
+                        <a class="fancybox" rel="group" href="img/products/product2.PNG">
+                            <img class="product_img img-portfolio img-responsive" src="img/products/product2.PNG">
+                        </a>
+                        <a class="fancybox" rel="group" href="img/products/product3.PNG">
+                            <img class="product_img img-portfolio img-responsive" src="img/products/product3.PNG">
+                        </a>
+                        <br>
+                        <h3></h3>
+<!--                        <div class="col-md-6">-->
+<!--                            <div class="portfolio-item">-->
+<!--                                <a href="#">-->
+<!--                                    <img class="img-portfolio img-responsive" src="img/portfolio-1.jpg">-->
+<!--                                </a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                        <div class="col-md-6">-->
+<!--                            <div class="portfolio-item">-->
+<!--                                <a href="#">-->
+<!--                                    <img class="img-portfolio img-responsive" src="img/portfolio-2.jpg">-->
+<!--                                </a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                        <div class="col-md-6">-->
+<!--                            <div class="portfolio-item">-->
+<!--                                <a href="#">-->
+<!--                                    <img class="img-portfolio img-responsive" src="img/portfolio-3.jpg">-->
+<!--                                </a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                        <div class="col-md-6">-->
+<!--                            <div class="portfolio-item">-->
+<!--                                <a href="#">-->
+<!--                                    <img class="img-portfolio img-responsive" src="img/portfolio-4.jpg">-->
+<!--                                </a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <!-- /.row (nested) -->
+<!--                    <a href="#" class="btn btn-dark">View More Items</a>-->
+                </div>
+                <!-- /.col-lg-10 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+
+    <!-- Call to Action -->
+    <aside class="call-to-action bg-primary">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h3>更多資訊</h3>
+                    <a href="http://www.hktdc.com/sourcing/hk_company_directory.htm?companyid=1X02VBTA&locale=zh_TW" target="_blank" class="btn btn-lg btn-light">貿發網</a>
+<!--                    <a href="#" class="btn btn-lg btn-dark">Look at Me!</a>-->
+                </div>
+            </div>
+        </div>
+    </aside>
+
+    <!-- Map -->
+    <section id="contact" class="map">
+        <iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=%E5%BB%A3%E6%9D%B1%E6%83%A0%E5%B7%9E%E5%B8%82.%E9%8E%AE%E9%9A%86%E9%8E%AE&key=AIzaSyBB6qe8t-Fs5Nj5hm4-fwHK1AqPt5ikHx4"></iframe>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                    <h4><strong>香港永佳實業有限公司</strong></h4>
+
+                    <p>香港 - 北角 電氣道233號 城市花園第10座13樓D室</p>
+
+                    <p>惠州 - 廣東惠州市.鎮隆鎮. 黃洞工業區廠: <br>興永佳塑膠製品(惠州)有限公司</p>
+                    <ul class="list-unstyled">
+                        <li><i class="fa fa-phone fa-fw"></i> (852) 2566-4066</li>
+                        <li><i class="fa fa-phone fa-fw"></i> (852) 2512-0339 -  Fax </li>
+                        <li><i class="fa fa-envelope-o fa-fw"></i>  <a href="mailto:manngais@yahoo.com.hk">manngais@yahoo.com.hk</a>
+                        </li>
+                    </ul>
+                    <br>
+<!--                    <ul class="list-inline">-->
+<!--                        <li>-->
+<!--                            <a href="#"><i class="fa fa-facebook fa-fw fa-3x"></i></a>-->
+<!--                        </li>-->
+<!--                        <li>-->
+<!--                            <a href="#"><i class="fa fa-twitter fa-fw fa-3x"></i></a>-->
+<!--                        </li>-->
+<!--                        <li>-->
+<!--                            <a href="#"><i class="fa fa-dribbble fa-fw fa-3x"></i></a>-->
+<!--                        </li>-->
+<!--                    </ul>-->
+                    <hr class="small">
+                    <p class="text-muted">Copyright &copy; Hong Kong Wing Kai <?php echo date("Y"); ?> All Rights Reserved.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+<!--    <script src="js/jquery.js"></script>-->
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script>
+    // Closes the sidebar menu
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+
+    // Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+
+    // Scrolls to the selected menu item on the page
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    </script>
+
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $(".fancybox").fancybox({
+                helpers: {
+                    overlay: {
+                        locked: false
+                    }
+                }
+            });
+        });
+    </script>
+</body>
+
+</html>
